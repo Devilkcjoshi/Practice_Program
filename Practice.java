8e9cf095f46db46cc845c20c4a496545e586f18b
Kamal chandra joshi <kj144049@gmail.com>
2024-09-09 10:40:29 +0530

    hello kamal

---
+++ Practice.java
@@ -0,0 +1,7 @@
+package PracticeProgram;
+
+public class Practice {
+    public static void main(String[] args) {
+        System.out.println("Hello friends this is kamal chandra joshi.");
+    }
+}
