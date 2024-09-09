584ada0eeea721d654447f8f90114f13e55a0037
Kamal chandra joshi <kj144049@gmail.com>
2024-09-09 10:49:29 +0530

    modefied again

--- Practice.java
+++ Practice.java
@@ -4,5 +4,6 @@ public class Practice {
     public static void main(String[] args) {
         System.out.println("Hello friends this is kamal chandra joshi.");
         System.out.println("Hello this is our second line of code.");
+        System.out.println("Hello this si our third line of code");
     }
 }
